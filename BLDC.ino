769283345bc71352be8118743bbfce46b8f2d3d7
Rishabh Agrawal <43053152+arishabh@users.noreply.github.com>
2018-09-08 20:38:26 -0400

    Add files via upload

---
+++ BLDC.ino
@@ -0,0 +1,22 @@
+#include<Servo.h>
+
+Servo esc;
+int val;
+
+void setup() {
+  pinMode(A0, INPUT);
+  esc.attach(8);
+  esc.write(30);
+  delay(500);
+  Serial.begin(9600);
+}
+
+void loop() {
+  val = analogRead(A0);
+  Serial.print("Pot Value: ");
+  Serial.println(val);
+  val=map(val, 0, 1023, 40, 130);
+  Serial.print("Drone Speed Value: ");
+  Serial.println(val);
+  esc.write(val);
+}
